ddb237f66a6e4a01bf64fb453339bd172810f8ec
Mike Heft <mikeheft@gmail.com>
2020-03-13 05:20:34 +0000

    Refact SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React    from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM      from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  state = {lat: null, errorMessage: ''}
 
-    this.state = {lat: null}
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({lat: position.coords.latitude})
-      },
-      error => console.log(error)
+      pos => this.setState({lat: pos.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
     )
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('UPDATED')
+  }
+
   render() {
-    return <div>Latitude: { this.state.lat }</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: { this.state.errorMessage }</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    return <div>Loading...</div>
   }
 }
 
